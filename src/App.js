9fea8cb512f4f8a3d69ecac4e33acebe1d8b4613
hendrick <h.lincertin@gmail.com>
2020-11-13 01:37:36 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, useReducer } from "react";
 import { ethers } from "ethers";
 import { isConnected2MetaMask } from "./utils/eth-utils";
-import { Citizen_address, Citizen_abi } from "./Contracts/Citizen";
+import { Citizen_address, Citizen_abi } from "./Contracts/Citizen.js";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="container my-4">
-      <h1 className="text-center">Citizen DApp</h1>
+    <div className="container">
+      <Register />;
     </div>
   );
 }
-
 export default App;
