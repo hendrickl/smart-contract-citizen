3a47961f6c365e0e5090f09b04219fd58eee82f8
hendrick <h.lincertin@gmail.com>
2020-11-15 16:42:12 +0100

    App.js import  updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect, useReducer } from "react";
-import { ethers } from "ethers";
-import { isConnected2MetaMask } from "./utils/eth-utils";
-import { Citizen_address, Citizen_abi } from "./contracts/Citizen.js";
+import React from "react";
 import RegisterWeb3 from "./components/RegisterWeb3";
 
 function App() {
